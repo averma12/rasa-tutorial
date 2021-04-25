9c5e69edc15838a3155ead288d5d868330cd4eef
Abhinav Verma <abhinav_win12@yahoo.co.in>
2021-04-26 02:56:19 +0530

    add methods of tracker object and slots

--- actions/actions.py
+++ actions/actions.py
@@ -36,7 +36,16 @@ class ActionCustomCusine(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Let me search some restaurants for you")
+
+        #print(tracker.slots)
+        print(tracker.get_slot("cuisine"))
+        print(tracker.get_intent_of_latest_message())
+        #print(tracker.events_after_latest_restart())
+        entity = tracker.get_slot("cuisine")
+        if entity in ["Hi","Hello"]:
+            dispatcher.utter_message(response="utter_greet")
+        else:
+            dispatcher.utter_message(text=f"Let me search some restaurants for {entity} cuisine for you")
 
         return []
 
